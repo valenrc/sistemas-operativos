02ac207bc2ccf00ed7efc14276db01e8fddd1617
valenrc <vvalenrc@gmail.com>
2025-09-22 14:02:49 -0300

    sockets-1

--- clases/IPC_templates/sockets-1/servidor.c
+++ clases/IPC_templates/sockets-1/servidor.c
@@ -11,8 +11,8 @@ int main() {
     int client_socket;
     struct sockaddr_un server_addr;
     struct sockaddr_un client_addr;
-    uint slen = sizeof(server_addr);
-    uint clen = sizeof(client_addr);
+    int slen = sizeof(server_addr);
+    int clen = sizeof(client_addr);
 
     server_addr.sun_family = AF_UNIX;
     strcpy(server_addr.sun_path, "unix_socket");
