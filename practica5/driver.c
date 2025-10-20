7e3b8418dbb2a4a7130d97df6a47880ae99d1f78
valenrc <vvalenrc@gmail.com>
2025-10-20 11:53:29 -0300

    drivers1

---
+++ practica5/driver.c
@@ -0,0 +1,10 @@
+driver_init(){
+}
+driver_open(){
+}
+driver_read(void* data){
+  // lee del hardware o buffer interno y escribe al usuario.
+}
+driver_write(void* data){
+  // lee datos del usuario y los envía al dispositivo
+}
